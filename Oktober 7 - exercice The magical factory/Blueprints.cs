94852bd6511566834b4c4ee0c0efb2dcb3898233
Mathias Angelin <mathias.angelin@studerande-skys.se>
2021-10-07 20:12:45 +0200

    My procejt

---
+++ Oktober 7 - exercice The magical factory/Blueprints.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oktober_7___exercice_The_magical_factory
+{
+    public enum Blueprints
+    {
+        Bicycle, WoodenHorse, Telephone
+    }
+}
---
+++ Oktober 7 - exercice The magical factory/Material.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+namespace Oktober_7___exercice_The_magical_factory
+
+{
+    public enum Material
+    {
+        Wood, Steel, Plastic, RedPaint, Screw
+    }
+
+}
+        
+
+
