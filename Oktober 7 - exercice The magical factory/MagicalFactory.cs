632299762f4a2240381a6fa4b142247c164017bd
Adam Lord <sildor55@gmail.com>
2021-10-13 18:14:28 +0200

    added MagicalFactory klass that will handle all the logic of the game

---
+++ Oktober 7 - exercice The magical factory/MagicalFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oktober_7___exercice_The_magical_factory
+{
+    public class MagicalFactory
+    {
+        private Fabric myFabric = new();
+        private Storage myStorage = new();
+        public MagicalFactory()
+        {
+
+        }
+        public void Run()
+        { 
+            while (true)
+            {
+
+
+                Console.WriteLine("Welcome to the Macical Factory!\n");
+
+                myStorage.ShowLager();
+                myFabric.ShowBlueprints();
+                myStorage.MaterialsFromStorageToFactory();
+
+                myFabric.ShowBlueprints();
+
+                
+                myStorage.ConvertMaterialsToInt();
+                myFabric.TryWoodenHorse(myStorage);
+                myFabric.TryTelephone(myStorage);
+                myFabric.TryBicycle(myStorage);
+                myStorage.ConvertUnusedIntsBackToMaterials();
+
+                myStorage.ShowLager();
+
+
+
+            }
+        }   
+    }
+}
--- Oktober 7 - exercice The magical factory/Program.cs
+++ Oktober 7 - exercice The magical factory/Program.cs
@@ -7,36 +7,13 @@ namespace Oktober_7___exercice_The_magical_factory
     {
         static void Main(string[] args)
         {
-
-
-
-            Fabric myFabric = new();
-            Storage myStorage = new();
-            while (true)
-            {
-
-
-                Console.WriteLine("Welcome to the Macical Factory!\n");
-
-                myStorage.ShowLager();
-                myFabric.ShowBlueprints();
-                myStorage.MaterialsFromStorageToFactory();
-
-                myFabric.ShowBlueprints();
-
-                
-                myStorage.ConvertMaterialsToInt();
-                myFabric.TryWoodenHorse(myStorage);
-                myFabric.TryTelephone(myStorage);
-                myFabric.TryBicycle(myStorage);
-                myStorage.ConvertUnusedIntsBackToMaterials();
-
-                myStorage.ShowLager();
-
-
-
-            }
+            MagicalFactory factory = new MagicalFactory();
+            factory.Run();
         }
+
+
+
+            
 
     }
 }
