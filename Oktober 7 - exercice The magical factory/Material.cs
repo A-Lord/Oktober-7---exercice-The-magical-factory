6cfd4ff2bf7536e60678e3e3d164722c02f7a2a9
MathiasAngelin <mathias.angelin@gmail.com>
2021-10-14 11:36:55 +0200

    Removed test

--- Oktober 7 - exercice The magical factory/Material.cs
+++ Oktober 7 - exercice The magical factory/Material.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 namespace Oktober_7___exercice_The_magical_factory
 
-{ // I'm just testing github/Mathias
+{
     public enum Material
     {
         Wood, Steel, Plastic, RedPaint, Screw
